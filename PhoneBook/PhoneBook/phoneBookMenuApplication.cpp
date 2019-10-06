56b6b90ba2adb016d9fa16c8155ba0e37c8947c6
nolanrdavenport <nolanrdavenport@gmail.com>
2019-10-05 20:40:20 -0500

    work

--- PhoneBook/PhoneBook/phoneBookMenuApplication.cpp
+++ PhoneBook/PhoneBook/phoneBookMenuApplication.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include "phoneBookEntry.h"
 using namespace std;
 
 int main() {
 	//TODO: Start working on the menu
 	cout << "Hello, world!" << endl;
+
+	phoneBookEntry entry("Nolan Davenport", "2812032172");
+	cout << entry.name();
 }
